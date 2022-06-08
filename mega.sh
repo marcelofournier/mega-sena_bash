d06513b2a2f61797e2f60322b37265676ec4c776
Marcelo Fournier <fournier.marcelo@gmail.com>
2022-06-08 15:52:58 -0400

    adicionadas algumas screenshots

--- mega.sh
+++ mega.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 #clear
 
+#**********************************   
+#
+#desenvolvido por Marcelo Fournier 
+#Email: fournier.marcelo@gmail.com
+#
+#**********************************   
+# Objetivo: brincar com bin/bash
+# Gerar 6 dezenas para você jogar na mega-sena brasileira
+#
+# Basicamente contem apenas 2 arquivos:
+#    1. mega.sh
+#    2. LEIAME.md
+# 
+# Não requer script ou bibliotecas adicionais
+# Por favor, abra o arquivo LEIAME.md para mais informações.
+#
+# Os arquivos *.bil são arquivos textos, referentes aos bilhetes da mega-sena gerados durante o processamento
+# Os arquivos *.dat são usados durante cada passo de processamento após a geração dos dados em si.
+# Ou seja, o processamento é feito em etapas:
+# 1 Passo. Gera os bilhetes
+# 2 Passo. Concatena todos os numeros sorteados dentro de um unico arquivo
+# 3 Passo. Agrupa cada numero pela quantidade de vezes que ele aparece, ou seja, foi sorteado
+# 4 Passo. Mostra apenas os 6 numeros mais sorteados
+# ns -> numeros sorteados
+#**********************************   
+
+
+
+#********************************
+# Apaga os arquivos *.bil - bilhetes gerados
+# e os arquivos *.dat 
+#********************************
+
 apaga_arquivos()
 {
 
@@ -13,6 +46,11 @@ apaga_arquivos()
     rm *.bil
 }
 
+
+#********************************
+# Chama o resultado do processamento e mostra ao usuario 
+#********************************
+
 mostra_resultado(){
     clear
     cat "$arquivo"
@@ -22,9 +60,8 @@ mostra_resultado(){
     echo "Bilhete sugerido:"
     echo "     x |número a jogar"
     #tail -n 6 ns5.dat
-    head -n 6 ns5.dat
-
-    
+    # mostra as 6 primeiras linhas do arquivo ns5.dat
+    head -n 6 ns5.dat   
 }
 
 
@@ -74,9 +111,13 @@ inicializa()
             #echo "=================================" >> resultado.dat
         done
 
+        #ordena os numeros do arquivo ns.dat e poe o resultado no ns2.dat
         sort -n ns.dat >> ns2.dat
+        #identifica os numeros unicos (sorteados) gerados no arquivo ns2.dat e insere no ns3.dat
         cat ns2.dat | uniq >> ns3.dat
+        #identifica e conta os arquivos unicos no arquivo ns2.dat e poe o resultado no ns4.dat
         uniq -c ns2.dat > ns4.dat
+        #ordena o quantificado arquivo ns4.dat e poe o resultado no ns5.dat
         sort -r ns4.dat > ns5.dat
         
 
